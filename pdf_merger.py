a9a4c8bf55b2856767e7eb954d8c426527b0fb68
saurabhag1 <72184067+saurabhag1@users.noreply.github.com>
2023-09-07 17:25:08 +0530

    Python Pdf Merge Project
    The Final Merge is in Merged.pdf with the Sample and 1.pdf to merge in Python

---
+++ pdf_merger.py
@@ -0,0 +1,11 @@
+# PDF MERGER
+
+import PyPDF2
+pdfiles =['1.pdf','sample.pdf']
+merger = PyPDF2.PdfMerger()
+for filename in pdfiles:
+    pdfFile=open(filename,'rb')
+    pdfReader= PyPDF2.PdfReader(pdfFile)
+    merger.append(pdfReader)
+pdfFile.close()
+merger.write('merged.pdf')
